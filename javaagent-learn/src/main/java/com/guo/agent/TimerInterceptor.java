c6237647bee5e0214dd4f055d1faaa91fa8d0f94
guolejian <guolejian@126.com>
2021-03-25 19:02:00 +0800

    fm

--- javaagent-learn/src/main/java/com/guo/agent/TimerInterceptor.java
+++ javaagent-learn/src/main/java/com/guo/agent/TimerInterceptor.java
@@ -11,12 +11,6 @@ import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 
-/**
- * @Date: 2021/2/4 14:08
- * @Author 郭乐建
- * @Since JDK 1.8
- * @Description:
- */
 public class TimerInterceptor {
 
     static ThreadLocal<Trace> traceLocal = new ThreadLocal<>();
@@ -24,53 +18,58 @@ public class TimerInterceptor {
     static Map<String, Trace> tracePointer = new ConcurrentHashMap<>();
 
     @RuntimeType
-    public static Object intercept(@This Object obj, @Origin Method method, @SuperCall Callable<?> callable) throws Exception {
-        Object result = null;
-        long threadId = Thread.currentThread().getId();
-        String key = null;
-        System.out.println("=============>method:"+method.getName());
-
-        Trace firstTrace = traceLocal.get();
-        if (firstTrace == null) {
-            // 说明是第一个节点
-            Trace trace = newTrace(method.getName(), "0", null,true);
-            key = trace.getTraceId();
-            tracePointer.put(trace.getTraceId(), trace);
-            traceLocal.set(trace);
-        } else {
-            Trace rootTrace = traceLocal.get();
-            Trace currentPointerTrace = tracePointer.get(rootTrace.getTraceId());
-            Trace trace = newTrace(method.getName(), currentPointerTrace.getSpanId(), rootTrace.getTraceId(),false);
-            // 设置当前指针指向最新得节点
-            tracePointer.put(rootTrace.getTraceId(), trace);
-
-            // 建立链表
-            currentPointerTrace.next = trace;
-            trace.prev = currentPointerTrace;
-
-            // 设置key
-            key = trace.getTraceId();
-        }
-
+    public static Object intercept(@Origin Method method, @SuperCall Callable<?> callable) throws Exception {
+        long start = System.currentTimeMillis();
         try {
-            result = callable.call(); // 执行原函数
-        } catch (Exception e) {
-            e.printStackTrace();
+            return callable.call();
         } finally {
-            Trace trace = tracePointer.get(key);
-            if (trace.isFirst()) {
-                trace.setEndTime(System.currentTimeMillis());
-                printTrace(trace);
-                // 一次打印结束
-                trace.next = null;
-                tracePointer.put(trace.getTraceId(), trace);
-            } else {
-                trace.setEndTime(System.currentTimeMillis());
-                tracePointer.put(trace.getTraceId(), trace.prev);
-            }
+            System.out.println(method.getName() +":"+(System.currentTimeMillis() - start)+"ms");
         }
-        return result;
     }
+//    @RuntimeType
+//    public static Object intercept(@This Object obj, @Origin Method method, @SuperCall Callable<?> callable) throws Exception {
+//        Object result = null;
+//        long threadId = Thread.currentThread().getId();
+//        String key = null;
+//        System.out.println("=============>method:"+method.getName());
+//
+//        Trace firstTrace = traceLocal.get();
+//        if (firstTrace == null) {
+//            Trace trace = newTrace(method.getName(), "0", null,true);
+//            key = trace.getTraceId();
+//            tracePointer.put(trace.getTraceId(), trace);
+//            traceLocal.set(trace);
+//        } else {
+//            Trace rootTrace = traceLocal.get();
+//            Trace currentPointerTrace = tracePointer.get(rootTrace.getTraceId());
+//            Trace trace = newTrace(method.getName(), currentPointerTrace.getSpanId(), rootTrace.getTraceId(),false);
+//            tracePointer.put(rootTrace.getTraceId(), trace);
+//
+//            currentPointerTrace.next = trace;
+//            trace.prev = currentPointerTrace;
+//
+//            key = trace.getTraceId();
+//        }
+//
+//        try {
+//            result = callable.call();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            Trace trace = tracePointer.get(key);
+//            if (trace.isFirst()) {
+//                trace.setEndTime(System.currentTimeMillis());
+//                printTrace(trace);
+//
+//                trace.next = null;
+//                tracePointer.put(trace.getTraceId(), trace);
+//            } else {
+//                trace.setEndTime(System.currentTimeMillis());
+//                tracePointer.put(trace.getTraceId(), trace.prev);
+//            }
+//        }
+//        return result;
+//    }
 
     private static void printTrace(Trace trace) {
         while (trace.next != null) {
